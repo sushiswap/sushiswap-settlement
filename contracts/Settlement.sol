5bb3efb91c22daf32b4b4c28e5dc0453643dc6d5
Team LevX <team@levx.app>
2020-10-04 01:19:33 +0900

    Update UniswapV2Router02 to directly call pairs Remove MasterChefDelegator

--- contracts/Settlement.sol
+++ contracts/Settlement.sol
@@ -4,15 +4,10 @@ pragma solidity =0.6.12;
 pragma experimental ABIEncoderV2;
 
 import "./BaseSettlement.sol";
-import "./UniswapV2Router02Delegator.sol";
-import "./MasterChefDelegator.sol";
+import "./UniswapV2Router02.sol";
 
-contract Settlement is BaseSettlement, UniswapV2Router02Delegator, MasterChefDelegator {
-    constructor(
-        IMasterChef _masterChef,
-        IMintable _rewardToken,
-        uint256 _rewardPerAmountFilled
-    ) public MasterChefDelegator(_masterChef) {
+contract Settlement is BaseSettlement, UniswapV2Router02 {
+    constructor(IMintable _rewardToken, uint256 _rewardPerAmountFilled) public {
         rewardToken = _rewardToken;
         rewardPerAmountFilled = _rewardPerAmountFilled;
     }

a11232b68e97ecb2df4f3e7365cf39dea7e5068e
Ubuntu <lukasczk@seznam.cz>
2020-10-28 12:01:52 +0000

    Add docs for the OrderBook & Settlement contracts

--- contracts/Settlement.sol
+++ contracts/Settlement.sol
@@ -19,22 +19,31 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
     using Bytes32Pagination for bytes32[];
 
     bool private _initialized;
+    // Array of hashes of all canceled orders
     bytes32[] internal _allCanceledHashes;
+    // Address of order maker => canceled hashes (orders)
     mapping(address => bytes32[]) internal _canceledHashesOfMaker;
+    // Address of fromToken => canceled hashes (orders)
     mapping(address => bytes32[]) internal _canceledHashesOfFromToken;
+    // Address of toToken => canceled hashes (orders)
     mapping(address => bytes32[]) internal _canceledHashesOfToToken;
+    // Hash of an order => if canceled
     mapping(bytes32 => bool) public canceledOfHash;
+    // Hash of an order => filledAmountIn
     mapping(bytes32 => uint256) public filledAmountInOfHash;
 
+    // Address of the Sushi token
     address public sushi;
 
-    // this address receives (feeSplitNumerator / 10000) of fee for every order filling
+    // This address receives (feeSplitNumerator / 10000) of fee for every order filling
     address public feeSplitRecipient;
 
-    // denominator is 10000
+    // Used to calculate the total fee of an order
+    // Denominator is 10000
     uint256 public feeNumerator;
 
-    // out of fee, denominator is 10000
+    // Used to calculate how big the share going to the relayer is
+    // Out of fee, denominator is 10000
     uint256 public feeSplitNumerator;
 
     function initialize(
@@ -59,27 +68,33 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
         _initialized = true;
     }
 
+    // Updates the fee amount and it's split ratio between the relayer and feeSplitRecipient
     function updateFee(uint256 _feeNumerator, uint256 _feeSplitNumerator) public onlyOwner {
         feeNumerator = _feeNumerator;
         feeSplitNumerator = _feeSplitNumerator;
     }
 
+    // Returns the number of canceled orders of a maker
     function numberOfCanceledHashesOfMaker(address maker) public view returns (uint256) {
         return _canceledHashesOfMaker[maker].length;
     }
 
+    // Return the number of canceled orders where fromToken is the origin token
     function numberOfCanceledHashesOfFromToken(address fromToken) public view returns (uint256) {
         return _canceledHashesOfFromToken[fromToken].length;
     }
 
+    // Return the number of canceled orders where toToken is the target token
     function numberOfCanceledHashesOfToToken(address toToken) public view returns (uint256) {
         return _canceledHashesOfToToken[toToken].length;
     }
 
+    // Returns the number of canceled orders
     function numberOfAllCanceledHashes() public view returns (uint256) {
         return _allCanceledHashes.length;
     }
 
+    // Returns an array of hashes of canceled orders of a maker
     function canceledHashesOfMaker(
         address maker,
         uint256 page,
@@ -88,6 +103,7 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
         return _canceledHashesOfMaker[maker].paginate(page, limit);
     }
 
+    // Returns an array of hashes of canceled orders where fromToken is the origin token
     function canceledHashesOfFromToken(
         address fromToken,
         uint256 page,
@@ -96,6 +112,7 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
         return _canceledHashesOfFromToken[fromToken].paginate(page, limit);
     }
 
+    // Returns an array of hashes of canceled orders where toToken is the target token
     function canceledHashesOfToToken(
         address toToken,
         uint256 page,
@@ -104,10 +121,12 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
         return _canceledHashesOfToToken[toToken].paginate(page, limit);
     }
 
+    // Return an array of canceled hashes
     function allCanceledHashes(uint256 page, uint256 limit) public view returns (bytes32[] memory) {
         return _allCanceledHashes.paginate(page, limit);
     }
 
+    // Returns the hash of the input arguments (which make an order)
     function hashOfOrder(
         address maker,
         address fromToken,
@@ -120,19 +139,23 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
         return Orders.hash(maker, fromToken, toToken, amountIn, amountOutMin, recipient, deadline);
     }
 
+    // Fills an order
     function fillOrder(FillOrderArgs memory args) public override returns (uint256 amountOut) {
         // solhint-disable-next-line avoid-tx-origin
         require(msg.sender == tx.origin, "called-by-contract"); // voids flashloan attack vectors
 
+        // Hash of the order
         bytes32 hash = args.order.hash();
+        // Check if the order is valid
         if (!_validateArgs(args, hash)) {
             return 0;
         }
+        // Check if the order is canceled / already fully filled
         if (!_validateStatus(args, hash)) {
             return 0;
         }
 
-        // calculates fee deducted amountIn and amountOutMin
+        // Calculates fee deducted amountIn and amountOutMin
         (uint256 amountIn, uint256 amountOutMin) = (
             args.amountToFillIn,
             args.order.amountOutMin.mul(args.amountToFillIn) / args.order.amountIn
@@ -144,7 +167,7 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
             amountOutMin = amountOutMin.sub(amountOutMin.mul(_feeNumerator) / 10000);
         }
 
-        // requires args.amountToFillIn to have already been approved to this
+        // Requires args.amountToFillIn to have already been approved to this
         amountOut = _swapExactTokensForTokens(
             args.order.maker,
             amountIn,
@@ -155,16 +178,17 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
 
         if (amountOut > 0) {
             if (fee > 0) {
-                _transferFees(args.order.fromToken, args.order.maker, fee, hash);
+                _transferFees(args.order.from=Token, args.order.maker, fee, hash);
             }
 
-            // this line is free from reentrancy issues since UniswapV2Pair prevents from them
+            // This line is free from reentrancy issues since UniswapV2Pair prevents from them
             filledAmountInOfHash[hash] = filledAmountInOfHash[hash].add(args.amountToFillIn);
 
             emit OrderFilled(hash, args.amountToFillIn, amountOut);
         }
     }
 
+    // Checks if an order is valid - if it contains all the information required
     function _validateArgs(FillOrderArgs memory args, bytes32 hash) internal view returns (bool) {
         return
             args.order.maker != address(0) &&
@@ -182,6 +206,7 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
             Verifier.verify(args.order.maker, hash, args.order.v, args.order.r, args.order.s);
     }
 
+    // Checks if an order is canceled / already fully filled
     function _validateStatus(FillOrderArgs memory args, bytes32 hash) internal view returns (bool) {
         if (canceledOfHash[hash]) {
             return false;
@@ -192,13 +217,14 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
         return true;
     }
 
+    // Transfers the fees to the feeSplitRecipient and the relayer
     function _transferFees(
         address fromToken,
         address maker,
         uint256 amount,
         bytes32 hash
     ) internal {
-        // if fromToken is WETH then path is [fromToken, sushi], otherwise [fromToken, WETH, sushi]
+        // If fromToken is WETH then path is [fromToken, sushi], otherwise [fromToken, WETH, sushi]
         address _weth = WETH;
         address[] memory path = new address[](fromToken == _weth ? 2 : 3);
         path[path.length - 1] = sushi;
@@ -220,6 +246,8 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
         emit FeeTransferred(hash, msg.sender, remainder);
     }
 
+    // Swaps an exact amount of tokens for another token through the path passed as an argument
+    // Returns the amount of the final token
     function _swapExactTokensForTokens(
         address from,
         uint256 amountIn,
@@ -241,6 +269,7 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
         amountOut = amounts[amounts.length - 1];
     }
 
+    // Fills multiple orders passed as an array
     function fillOrders(FillOrderArgs[] memory args)
         public
         override
@@ -249,14 +278,17 @@ contract Settlement is Ownable, UniswapV2Router02Settlement {
         bool filled = false;
         amountsOut = new uint256[](args.length);
         for (uint256 i = 0; i < args.length; i++) {
+            // Returns zero of the order wasn't filled
             amountsOut[i] = fillOrder(args[i]);
             if (amountsOut[i] > 0) {
+                // At least one order was filled
                 filled = true;
             }
         }
         require(filled, "no-order-filled");
     }
 
+    // Cancels an order, has to been called by order maker
     function cancelOrder(Orders.Order memory order) public override {
         bytes32 hash = order.hash();
         // it's not required to verify the signature of the order
